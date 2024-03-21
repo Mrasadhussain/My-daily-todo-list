5d8812ea38e6ab299757ed6f58d8431507bd4de9
Mrasadhussain <asadsmit11@gmail.com>
2024-03-21 20:27:16 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,20 @@
+// document.querySelector("#push").onclick = function() {
+//     var taskInput = document.querySelector("#newtask input");
+
+//     if (taskInput.value.trim().length == 0) {
+//         alert("Please Enter a Task");
+//     } else {
+//         document.querySelector("#task").innerHTML += `
+//         <div class="task">
+//             <span class="taskname">
+//                 ${taskInput.value}
+//             </span>
+//             <button class="delete">
+//                 <i class="far fa-trash-alt"></i>
+//             </button>
+//         </div>
+//         `;
+//     }
+//     // Clear input after adding task
+//     taskInput.value = "";
+// };
